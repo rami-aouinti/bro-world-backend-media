a55a5240c6298735f00f773a01948895f5ed233a
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-07-05 03:28:44 +0200

    remove unused functions

---
+++ src/Media/Transport/Controller/Frontend/DeleteMediaFolderController.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Media\Transport\Controller\Frontend;
+
+use App\General\Domain\Utils\JSON;
+use App\Media\Domain\Entity\Media;
+use App\Media\Domain\Entity\MediaFolder;
+use Doctrine\ORM\EntityManagerInterface;
+use JsonException;
+use Nelmio\ApiDocBundle\Attribute\Model;
+use OpenApi\Attributes as OA;
+use OpenApi\Attributes\JsonContent;
+use OpenApi\Attributes\Property;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * @package App\Media
+ */
+#[AsController]
+#[OA\Tag(name: 'Media')]
+readonly class DeleteMediaFolderController
+{
+    public function __construct(
+        private SerializerInterface $serializer,
+        private EntityManagerInterface $entityManager
+    ) {
+    }
+
+    /**
+     * Get current user media data, accessible only for 'IS_AUTHENTICATED_FULLY' users.
+     *
+     * @throws JsonException
+     * @throws ExceptionInterface
+     */
+    #[Route(
+        path: '/v1/platform/mediaFolder/{folder}',
+        methods: [Request::METHOD_DELETE],
+    )]
+    public function __invoke(MediaFolder $folder): JsonResponse
+    {
+        $this->entityManager->remove($folder);
+        $this->entityManager->flush();
+
+        /** @var array<string, string|array<string, string>> $output */
+        $output = JSON::decode(
+            $this->serializer->serialize(
+                'success',
+                'json',
+                [
+                    'groups' => 'Media',
+                ]
+            ),
+            true,
+        );
+
+        return new JsonResponse($output);
+    }
+}
